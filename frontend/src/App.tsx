4f80115b65d236249399cecde6d8edeea95105ec
Bruno A. Muciño <mucinoab@gmail.com>
2024-01-08 21:46:26 -0600

    Basic react/vite setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,26 @@
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import Checklist from "./components/Checklist.tsx";
+
+export default () => (
+  <Tabs>
+    <TabList>
+      <Tab>Itinerary</Tab>
+      <Tab>Check List</Tab>
+      <Tab>Split Costs</Tab>
+    </TabList>
+
+    <TabPanel>
+      <Checklist />
+    </TabPanel>
+
+    <TabPanel>
+      <h2>Any content 1</h2>
+    </TabPanel>
+
+    <TabPanel>
+      <h2>Any content 2</h2>
+    </TabPanel>
+  </Tabs>
+);
+
---
+++ frontend/src/components/Checklist.tsx
@@ -0,0 +1,6 @@
+export default () => {
+  // Get check list items from back end.
+  let a = 0;
+
+  return <p> {a} </p >;
+};
