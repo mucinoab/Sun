bd3d57a9457e26b180ba41045f97b60eb76689b7
Bruno A. Muciño <mucinoab@gmail.com>
2024-01-12 18:11:55 -0600

    Add debounce and ability to modify content of check list item

--- frontend/src/components/ChecklistItem.tsx
+++ frontend/src/components/ChecklistItem.tsx
@@ -11,27 +11,31 @@ export default (props: { id: string, item: Item }) => {
       return;
     }
 
-    // Only save state when updating the initial state
-    fetch(`item/${props.id}`, {
-      method: "POST",
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(item),
-    });
+    const timeoutId = setTimeout(() => {
+      // Only save state when updating the initial state and only after multiple
+      // updates in a short time period.
+      fetch(`item/${props.id}`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(item),
+      });
+    }, 300);
+
+    return () => clearTimeout(timeoutId); // Clean up last timeout.
   }, [item.complete, item.ordinality, item.content]);
 
   return <li>
     <label >
-      <input type="checkbox" defaultChecked={item.complete} onChange={e => handleChange(e)} />
-      {item.content}
+      <input type="checkbox" defaultChecked={item.complete} onChange={() => handleChange()} />
+      <input type="input" value={item.content?.toString()} onInput={e => handleInput(e)} />
     </label>
   </li>;
 
-  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-    if (e.target.type === "checkbox") {
-      setItem(i => {
-        i.complete = !i.complete;
-        return i;
-      });
-    }
+  function handleChange() {
+    setItem(i => { return { ...i, complete: !i.complete }; });
+  }
+
+  function handleInput(e: any) {
+    setItem(i => { return { ...i, content: e.target.value }; });
   }
 };
